111a9d0ecbf8081bdb3168890fd25f9cc2e1934e
Jason Hua <jayhua333@gmail.com>
2015-04-30 15:33:36 -0700

    Completed numbers and variables

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,17 +12,24 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
+    number++;
     return number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    return number1;
+    NSInteger addNumber = number1;
+    NSInteger toNumber = number2;
+    NSInteger totalNum = addNumber + toNumber;
+    return totalNum;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    NSInteger remainderOfNumber = dividend;
+    NSInteger dividedByNumber = divisor;
+    NSInteger equation = remainderOfNumber % dividedByNumber;
+    return equation;
 }
 
 @end
