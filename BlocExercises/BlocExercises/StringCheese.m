42bb6bf59e71427f1dff6ed3669b174a8dbeda3e
Jason Hua <jayhua333@gmail.com>
2015-05-01 01:29:11 -0700

    Completed strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,19 +12,49 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *fullSentence = [NSString stringWithFormat:@"My favorite cheese is %@.",cheeseName];
+    return fullSentence;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *shortName = cheeseName;
+    NSRange cheeseRange = [shortName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    
+    //if cheese is not found, then keep shortName
+    if (cheeseRange.length == 0) {
+        return shortName;
+    }
+    
+    NSString *realShortName = [shortName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+    
+    return realShortName;
 }
 
+//- (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+//    /* WORK HERE */
+//    NSString *shortName = cheeseName;
+//    NSRange cheeseRange = [shortName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+//    
+//    //if cheese is not found, then keep shortName
+//    if (cheeseRange.location != NSNotFound) {
+//        
+//        shortName = [shortName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+//    }
+//
+//    return shortName;
+//}
+
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
-    if (cheeseCount == 1) {
+    if (cheeseCount == 1) { //if cheeseCount equals 1, then 1 cheese
+        NSInteger one = cheeseCount;
+        NSString *phrase = [NSString stringWithFormat:@"%ld cheese",(long)one];
+        return phrase;
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
     } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSInteger moreThanOne = cheeseCount;
+        NSString *phrase2 = [NSString stringWithFormat:@"%ld cheeses",(long)moreThanOne];
+        return phrase2;
     }
     
     /*
